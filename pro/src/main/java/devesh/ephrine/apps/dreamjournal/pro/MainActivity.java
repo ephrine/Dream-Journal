52bda7da2da8513448bc90d98f6b2059b40b11eb
haseebpvt <haseebpvt@gmail.com>
2018-11-09 22:01:08 +0530

    Test commit

--- pro/src/main/java/devesh/ephrine/apps/dreamjournal/pro/MainActivity.java
+++ pro/src/main/java/devesh/ephrine/apps/dreamjournal/pro/MainActivity.java
@@ -65,9 +65,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, CreateDreamActivity.class);
                 startActivity(intent);
-
-                //     Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-             //           .setAction("Action", null).show();
             }
         });
 
